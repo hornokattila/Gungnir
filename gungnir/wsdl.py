525ba0ceeac9904b9df7f93f4b4d0a371e3217af
hornokattila <NSt94cnrs>
2019-06-22 23:30:14 +0200

    Empty util folder.

--- gungnir/wsdl.py
+++ gungnir/wsdl.py
@@ -4,7 +4,7 @@ import flask
 
 
 class Flask(flask.Flask):
-    def run(self, **options: typing.Union[str, typing.Dict[str, typing.Union[bool, int, str]], typing.List[flask.Blueprint]]) -> None:
+    def run(self: typing.Type["Flask"], **options: typing.Union[str, typing.Dict[str, typing.Union[bool, int, str]], typing.List[flask.Blueprint]]) -> None:
         version = options.pop("version")
         for url in options.pop("urls"):
             super().register_blueprint(url, url_prefix=version)

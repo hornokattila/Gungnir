73e38c73fad4da7fe251780f8dbed383ba9352a3
hornokattila <NSt94cnrs>
2019-07-08 23:30:43 +0200

    Singleton executor.

--- gungnir/services/Executor.py
+++ gungnir/services/Executor.py
@@ -4,3 +4,11 @@ from concurrent.futures.thread import ThreadPoolExecutor
 class Executor(ThreadPoolExecutor):
     def __init__(self) -> None:
         super().__init__(1)
+
+    def __new__(cls) -> "Executor":
+        if not executor:
+            return super().__new__(cls)
+        return executor
+
+
+executor: Executor = Executor()

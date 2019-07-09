dba1b795a1ef833a22b125e78c794ef74a8de7e7
hornokattila <NSt94cnrs>
2019-07-09 20:56:14 +0200

    Removed status API because it will not need.

--- gungnir/urls.py
+++ gungnir/urls.py
@@ -2,7 +2,6 @@ import typing
 
 from api.health import health
 from api.logger import logger
-from api.status import status
 from api.submit import submit
 from api.upload import upload
 from utils.Blueprint import Blueprint
@@ -11,7 +10,6 @@ version: str = "/v1"
 urls: typing.List[Blueprint] = [
     health,
     logger,
-    status,
     submit,
     upload
 ]

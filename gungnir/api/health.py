b62ed8d349327516d2c938baefd96278d294192b
hornokattila <NSt94cnrs>
2019-08-19 22:13:21 +0200

    Inform skeleton.

--- gungnir/api/health.py
+++ gungnir/api/health.py
@@ -9,6 +9,11 @@ class Health(Blueprint):
 health: Health = Health("health", __name__)
 
 
+@health.route("/inform")
+def _inform() -> str:
+    return ""
+
+
 @health.route("/health")
 def _health() -> str:
     return ""

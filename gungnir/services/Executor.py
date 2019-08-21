fe31bdc12e06094a3f636862d62fc2858cc4f093
hornokattila <NSt94cnrs>
2019-08-21 22:16:50 +0200

    Variable usage simplification.

--- gungnir/services/Executor.py
+++ gungnir/services/Executor.py
@@ -16,12 +16,11 @@ class Executor:
             self.validate(json)
             os.makedirs(self.submit_folder)
             name: str = uuid.uuid4().get_hex()
-            logger_path: str = os.path.join(self.logger_folder, "{0}.log".format(name))
-            submit_path: str = os.path.join(self.submit_folder, "{0}.bat".format(name))
-            with open(submit_path, "x") as file:
+            path: str = os.path.join(self.submit_folder, "{0}.bat".format(name))
+            with open(path, "x") as file:
                 file.write(json["script"])
-            self.executor.submit(os.system, "{0} {1} > {2}".format(submit_path, self.upload_folder, logger_path))
-            self.executor.submit(os.remove, submit_path)
+            self.executor.submit(os.system, "{0} {1} > {2}".format(path, self.upload_folder, os.path.join(self.logger_folder, "{0}.log".format(name))))
+            self.executor.submit(os.remove, path)
             self.executor.submit(os.removedirs, self.submit_folder)
             return name
         except OSError:

f5e38638d5b723a57358fc72034dabdc23761d3b
hornokattila <NSt94cnrs>
2019-08-27 22:09:00 +0200

    Halfway done setuptools packaging description.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    dependency_links=None,
+    setup_requires=None,
+    distclass=None,
+    script_name=None,
+    script_args=None,
+    options=None,
+    name="Gungnir",
+    version="1.0.0",
+    author="Attila Hornok",
+    author_email="hornokattila@outlook.com",
+    maintainer="Attila Hornok",
+    maintainer_email="hornokattila@outlook.com",
+    url="https://github.com/Myrkheim/Gungnir",
+    license=None,
+    description=None,
+    long_description=None,
+    keywords=None,
+    platforms=None,
+    classifiers=None,
+    download_url="https://github.com/Myrkheim/Gungnir/archive/master.zip",
+    requires=None,
+    provides=None,
+    obsoletes=None
+)

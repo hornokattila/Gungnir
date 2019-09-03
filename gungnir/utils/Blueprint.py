cedc4f99ecc8ccfce695ece36a3bb66cf1380366
hornokattila <NSt94cnrs>
2019-09-04 01:00:14 +0200

    Initial security configuration.

--- gungnir/utils/Blueprint.py
+++ gungnir/utils/Blueprint.py
@@ -12,12 +12,16 @@ class Blueprint(abc.ABC, flask.Blueprint):
     executor: ThreadPoolExecutor = ThreadPoolExecutor(1)
 
     def __init__(self, name: str, import_name: str) -> None:
+        super().__init__(name, import_name)
+        self.before_request(self.check_password)
         self.flask: flask = flask
         self.settings: typing.Dict[str, typing.Union[bool, int, str]] = settings.settings["urls"]
         self.werkzeug: werkzeug = werkzeug
-        super().__init__(name, import_name)
         self.init()
 
     @abc.abstractmethod
     def init(self) -> None:
+        pass
+
+    def check_password(self) -> None:
         pass

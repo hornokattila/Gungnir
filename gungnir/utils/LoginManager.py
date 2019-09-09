7e4d4b4817214fd9176035239fe182791ca6aa18
hornokattila <NSt94cnrs>
2019-09-09 21:23:11 +0200

    HTTP Basic auth is started.

--- gungnir/utils/LoginManager.py
+++ gungnir/utils/LoginManager.py
@@ -1,6 +1,10 @@
+import base64
 import typing
 
 
 class LoginManager:
     def ba_spwd(self, headers: typing.Dict[str, str]) -> None:
-        pass
+        try:
+            api_key: str = base64.b64decode(headers["Authorization"].replace("Basic ", "", 1))
+        except (KeyError, TypeError):
+            pass

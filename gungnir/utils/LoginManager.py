de375f35647faee5d7d511ceae609a0f13f58eb4
hornokattila <NSt94cnrs>
2019-09-10 20:01:55 +0200

    Renamed credentials in LoginManager.

--- gungnir/utils/LoginManager.py
+++ gungnir/utils/LoginManager.py
@@ -5,8 +5,8 @@ import typing
 class LoginManager:
     def ba_spwd(self, headers: typing.Dict[str, str]) -> None:
         try:
-            api_key: typing.List[str] = []
+            credentials: typing.List[str] = []
             for data in headers.get("Authorization").replace("Basic ", "", 1).split(":"):
-                api_key.append(base64.b64decode(data))
+                credentials.append(base64.b64decode(data))
         except TypeError:
             pass

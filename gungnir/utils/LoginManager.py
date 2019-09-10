f6a6b8da1b05c1d99343c15ed7e000955697b108
hornokattila <NSt94cnrs>
2019-09-10 19:55:54 +0200

    Improved LoginManager.

--- gungnir/utils/LoginManager.py
+++ gungnir/utils/LoginManager.py
@@ -5,6 +5,8 @@ import typing
 class LoginManager:
     def ba_spwd(self, headers: typing.Dict[str, str]) -> None:
         try:
-            api_key: str = base64.b64decode(headers["Authorization"].replace("Basic ", "", 1))
-        except (KeyError, TypeError):
+            api_key: typing.List[str] = []
+            for data in headers.get("Authorization").replace("Basic ", "", 1).split(":"):
+                api_key.append(base64.b64decode(data))
+        except TypeError:
             pass

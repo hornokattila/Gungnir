9099f6a05889e8a11384dbdc076dd8b3faad47ea
Attila Hornok <vaskaktusz@gmail.com>
2019-09-11 09:26:48 +0200

    Update LoginManager.py

--- gungnir/utils/LoginManager.py
+++ gungnir/utils/LoginManager.py
@@ -7,7 +7,7 @@ class LoginManager:
     def ba_spwd(self, headers: typing.Dict[str, str]) -> None:
         try:
             credentials: typing.List[str] = []
-            for data in headers.get("Authorization").replace("Basic ", "", 1).split(":"):
-                credentials.append(base64.b64decode(data))
+            for code in headers.get("Authorization").replace("Basic ", "", 1).split(":"):
+                credentials.append(base64.b64decode(code))
         except TypeError:
             pass

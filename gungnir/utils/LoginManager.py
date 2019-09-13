0ec865050ccbba420e4d2cb2d0473fd6618836b9
hornokattila <NSt94cnrs>
2019-09-13 19:13:16 +0200

    Improved Base64 decoding.

--- gungnir/utils/LoginManager.py
+++ gungnir/utils/LoginManager.py
@@ -6,8 +6,6 @@ import typing
 class LoginManager:
     def ba_spwd(self, headers: typing.Dict[str, str]) -> None:
         try:
-            credentials: typing.List[str] = []
-            for code in headers.get("Authorization").replace("Basic ", "", 1).split(":"):
-                credentials.append(base64.b64decode(code))
+            credentials: typing.List[str] = list(map(lambda code: base64.b64decode(code), headers.get("Authorization").replace("Basic ", "", 1).split(":")))
         except TypeError:
             pass

c483fd47b5e3584fd5132fba795f5a8f45bcc8de
hornokattila <NSt94cnrs>
2019-09-13 20:06:34 +0200

    Platform dependent auth intro.

--- gungnir/utils/LoginManager.py
+++ gungnir/utils/LoginManager.py
@@ -1,4 +1,5 @@
 import base64
+import sys
 import typing
 
 
@@ -6,6 +7,12 @@ import typing
 class LoginManager:
     def ba_spwd(self, headers: typing.Dict[str, str]) -> None:
         try:
-            credentials: typing.List[str] = base64.b64decode(headers.get("Authorization").replace("Basic ", "", 1)).decode().split(":")
+            getattr(self, sys.platform)(base64.b64decode(headers.get("Authorization").replace("Basic ", "", 1)).decode().split(":"))
         except TypeError:
             pass
+
+    def linux(self, credentials: typing.Dict[str, str]) -> None:
+        pass
+
+    def win32(self, credentials: typing.Dict[str, str]) -> None:
+        pass

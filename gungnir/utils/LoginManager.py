cb6d12176dbd0a3a763bf65589cba0f17e723953
hornokattila <NSt94cnrs>
2019-09-14 21:01:21 +0200

    Fixed error handling in LoginManager.

--- gungnir/utils/LoginManager.py
+++ gungnir/utils/LoginManager.py
@@ -3,12 +3,11 @@ import sys
 import typing
 
 
-# TODO: Work in progress!
 class LoginManager:
     def basicAuthAgainstSysInfo(self, headers: typing.Dict[str, str]) -> None:
         try:
             getattr(self, "_sys_{0}".format(sys.platform))(base64.b64decode(headers.get("Authorization").replace("Basic ", "", 1)).decode().split(":"))
-        except TypeError:
+        except (AttributeError, ValueError):
             pass
 
     def _sys_linux(self, credentials: typing.Dict[str, str]) -> None:

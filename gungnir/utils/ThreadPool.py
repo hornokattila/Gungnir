62ace0f9231b11853145d9bb4920e62621182e17
hornokattila <NSt94cnrs>
2019-09-21 17:59:37 +0200

    Sekelton enter and exit methods for ThreadPool.

--- gungnir/utils/ThreadPool.py
+++ gungnir/utils/ThreadPool.py
@@ -5,5 +5,11 @@ from concurrent.futures.thread import ThreadPoolExecutor
 class ThreadPool(abc.ABC):
     executor: ThreadPoolExecutor = ThreadPoolExecutor(1)
 
+    def __enter__(self) -> "ThreadPool":
+        return self
+
+    def __exit__(self, *exc) -> bool:
+        return True
+
     def __init__(self, submit_folder: str) -> None:
         self.submit_folder: str = submit_folder

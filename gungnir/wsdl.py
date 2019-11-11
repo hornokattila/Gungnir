cc74b90e3e4593fabb13a2b8d140ce718fc8f1a5
hornokattila <NSt94cnrs>
2019-11-11 22:02:23 +0100

    Renamed loop variable in wsdl.

--- gungnir/wsdl.py
+++ gungnir/wsdl.py
@@ -8,6 +8,6 @@ from utils.Blueprint import Blueprint
 class Flask(flask.Flask):
     def run(self, **options: typing.Union[str, typing.Dict[str, typing.Union[bool, int, str]], typing.List[Blueprint]]) -> None:
         url_prefix: str = options.pop("url_prefix")
-        for url in options.pop("urls"):
-            super().register_blueprint(url, url_prefix=url_prefix)
+        for prop in options.pop("urls"):
+            super().register_blueprint(prop, url_prefix=url_prefix)
         super().run(**options)

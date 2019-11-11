4e939778a0aac3579f9e6e76e6f09689499e4da7
hornokattila <NSt94cnrs>
2019-11-11 21:40:35 +0100

    Renamed key property.

--- gungnir/utils/ThreadPool.py
+++ gungnir/utils/ThreadPool.py
@@ -7,6 +7,6 @@ class ThreadPool(abc.ABC):
     executor: ThreadPoolExecutor = ThreadPoolExecutor(1)
 
     def validate(self, json: typing.Dict[str, str], keys: typing.List[str]) -> None:
-        for key in keys:
-            if key not in json:
+        for prop in keys:
+            if prop not in json:
                 raise ProcessLookupError()

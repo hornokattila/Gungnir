c5e89b7276b72aefcaf220952319fe4ce996c44b
hornokattila <NSt94cnrs>
2019-11-23 01:45:28 +0100

    Improved error handling.

--- gungnir/utils/LoginManager.py
+++ gungnir/utils/LoginManager.py
@@ -7,7 +7,7 @@ class LoginManager:
     def user_loader(self, headers: typing.Dict[str, str]) -> None:
         try:
             getattr(self, "_sys_{0}".format(sys.platform))(*base64.b64decode(headers.get("Authorization").replace("Basic ", "", 1)).decode().split(":"))
-        except (AttributeError, ValueError):
+        except (AttributeError, TypeError, ValueError):
             raise ProcessLookupError()
 
     def _sys_linux(self, username: str, password: str) -> None:

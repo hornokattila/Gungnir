c07870c65f60fe5136cb44bce958598c71d018fa
hornokattila <NSt94cnrs>
2019-12-15 11:29:34 +0100

    Changed HEalth end point name from system to device.

--- gungnir/api/health.py
+++ gungnir/api/health.py
@@ -11,19 +11,19 @@ class Health(Blueprint):
 
     def spec(self) -> typing.Dict[str, typing.Dict[str, typing.Union[str, typing.List[str]]]]:
         return {
-            "_health": {"rule": "/health", "methods": ["GET"]},
-            "_system": {"rule": "/system", "methods": ["GET"]}
+            "_device": {"rule": "/device", "methods": ["GET"]},
+            "_health": {"rule": "/health", "methods": ["GET"]}
         }
 
 
 health: Health = Health(LoginManager().system_loader)
 
 
+@health.route(**health.spec()["_device"])
+def _device() -> str:
+    return health.flask.json.dumps(dict(zip(("sysname", "nodename", "release", "version", "machine"), os.uname())))
+
+
 @health.route(**health.spec()["_health"])
 def _health() -> str:
     return ""
-
-
-@health.route(**health.spec()["_system"])
-def _system() -> str:
-    return health.flask.json.dumps(dict(zip(("sysname", "nodename", "release", "version", "machine"), os.uname())))

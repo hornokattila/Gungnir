faf8c55093216bb5d4207b9a5041d1b5373c28b4
hornokattila <NSt94cnrs>
2019-12-15 11:45:37 +0100

    Spearate system end point for API documentation.

--- gungnir/gui/visual.py
+++ gungnir/gui/visual.py
@@ -9,15 +9,23 @@ class Visual(Blueprint):
         pass
 
     def spec(self) -> typing.Dict[str, typing.Dict[str, typing.Union[str, typing.List[str]]]]:
-        return {"_visual": {"rule": "/", "methods": ["GET"]}}
+        return {
+            "_system": {"rule": "/system", "methods": ["GET"]},
+            "_visual": {"rule": "/", "methods": ["GET"]}
+        }
 
 
 visual: Visual = Visual(LoginManager().vacant_loader)
 
 
-@visual.route(**visual.spec()["_visual"])
-def _visual() -> str:
+@visual.route(**visual.spec()["_system"])
+def _system() -> str:
     spec: typing.Dict[str, typing.Dict[str, typing.Union[str, typing.List[str]]]] = {}
     for rule in visual.mirror:
         spec.update(rule.spec())
-    return visual.flask.render_template("visual.html", spec=spec, **visual.system)
+    return visual.flask.json.dumps(spec)
+
+
+@visual.route(**visual.spec()["_visual"])
+def _visual() -> str:
+    return visual.flask.render_template("visual.html", system=_system(), **visual.system)

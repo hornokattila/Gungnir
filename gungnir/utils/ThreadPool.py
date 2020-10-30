781a04724f000acefa42d1a5887cea956be5f685
hornokattila <hornokattila@outlook.com>
2020-10-30 23:35:30 +0100

    TODO: FIXME!

--- gungnir/utils/ThreadPool.py
+++ gungnir/utils/ThreadPool.py
@@ -1,3 +1,4 @@
+import os
 import typing
 from concurrent.futures.thread import ThreadPoolExecutor
 
@@ -6,8 +7,10 @@ executor: ThreadPoolExecutor = ThreadPoolExecutor(1)
 
 class ThreadPool:
     @staticmethod
-    def submit(rule: typing.Callable[[str], typing.Union[int, None]], args: str) -> None:
-        executor.submit(rule, args)
+    def submit(shebang: str, file: str, upload: str, logger: str) -> None:
+        # TODO: FIXME!
+        executor.submit(os.system, "{0} {1} {2} > {3}".format(shebang, file, upload, logger))
+        executor.submit(os.remove, file)
 
     @staticmethod
     def verify(json: typing.Dict[str, str], keys: typing.List[str]) -> None:

ad8d62d7531b71b2f86811d23e824ef459ad5641
Attila Hornok <hornokattila@outlook.com>
2022-11-23 17:11:59 +0100

    Renamed cert folder 2.

--- application.sh
+++ application.sh
@@ -2,10 +2,10 @@
 
 FOLDER=$(dirname "$0")
 export BUCKET=${FOLDER}/.pytest_cache
-export PUB=${BUCKET}/signed/pub.pem
-export KEY=${BUCKET}/signed/key.pem
+export PUB=${BUCKET}/secret/pub.pem
+export KEY=${BUCKET}/secret/key.pem
 
-mkdir -p "${BUCKET}"/signed
+mkdir -p "${BUCKET}"/secret
 [ -f "${PUB}" ] && [ -f "${KEY}" ] || openssl req -x509 -newkey rsa:4096 -nodes -out "${PUB}" -keyout "${KEY}"
 
 python3 "${FOLDER}"/manage.py

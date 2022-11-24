85dbf67f37286762f94c2cded657abd7d102282b
Attila Hornok <hornokattila@outlook.com>
2022-11-24 21:40:51 +0100

    Final application.sh script.

--- application.sh
+++ application.sh
@@ -2,22 +2,24 @@
 
 FOLDER=$(dirname "$0")
 export BUCKET=${FOLDER}/.pytest_cache
-export PUB=${BUCKET}/secret/pub.pem
-export KEY=${BUCKET}/secret/key.pem
+SECRET=${BUCKET}/secret
+PUB=${SECRET}/pub.pem
+KEY=${SECRET}/key.pem
 
-mkdir -p "${BUCKET}"/secret
-if [[ ! -f "${BUCKET}"/secret/secret.dmg ]]; then
+mkdir -p "${SECRET}"
+if [[ ! -f "${SECRET}"/secret.dmg ]]; then
   openssl req -x509 -newkey rsa:4096 -nodes -out "${PUB}" -keyout "${KEY}"
-  hdiutil create -encryption -stdinpass -srcfolder "${BUCKET}"/secret "${BUCKET}"/secret/secret.dmg
+  hdiutil create -encryption -stdinpass -srcfolder "${SECRET}" "${SECRET}"/secret.dmg
   rm "${PUB}"
   rm "${KEY}"
 fi
 
-if [[ ! -d /Volumes/secret ]]; then
-  hdiutil attach -stdinpass "${BUCKET}"/secret/secret.dmg
-fi
+VOLUME=/Volumes/secret
+export PUB=${VOLUME}/pub.pem
+export KEY=${VOLUME}/key.pem
 
-export PUB=/Volumes/secret/pub.pem
-export KEY=/Volumes/secret/key.pem
+if [[ ! -d ${VOLUME} ]]; then
+  hdiutil attach -stdinpass "${SECRET}"/secret.dmg
+fi
 
 python3 "${FOLDER}"/manage.py

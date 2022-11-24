aa3d141e660f752ca1057a2e7e82e6108bf9aa07
Attila Hornok <hornokattila@outlook.com>
2022-11-24 03:12:45 +0100

    Initial version of dmg file based cert handling.

--- application.sh
+++ application.sh
@@ -6,8 +6,18 @@ export PUB=${BUCKET}/secret/pub.pem
 export KEY=${BUCKET}/secret/key.pem
 
 mkdir -p "${BUCKET}"/secret
-if [[ ! -f "${PUB}" ]] && [[ ! -f "${KEY}" ]]; then
+if [[ ! -f "${BUCKET}"/secret/secret.dmg ]]; then
   openssl req -x509 -newkey rsa:4096 -nodes -out "${PUB}" -keyout "${KEY}"
+  hdiutil create -encryption -stdinpass -srcfolder "${BUCKET}"/secret "${BUCKET}"/secret/secret.dmg
+  rm "${PUB}"
+  rm "${KEY}"
 fi
+
+if [[ ! -d /Volumes/secret ]]; then
+  hdiutil attach -stdinpass "${BUCKET}"/secret/secret.dmg
+fi
+
+export PUB=/Volumes/secret/pub.pem
+export KEY=/Volumes/secret/key.pem
 
 python3 "${FOLDER}"/manage.py

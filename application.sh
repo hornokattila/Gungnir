6fd37366298950e744ea499d2414244b441e40d6
Attila Hornok <hornokattila@outlook.com>
2022-11-24 23:46:23 +0100

    Removed passworld parameter.

--- application.sh
+++ application.sh
@@ -9,7 +9,7 @@ KEY=${SECRET}/key.pem
 mkdir -p "${SECRET}"
 if [[ ! -f "${SECRET}"/secret.dmg ]]; then
   openssl req -x509 -newkey rsa:4096 -nodes -out "${PUB}" -keyout "${KEY}"
-  hdiutil create -encryption -stdinpass -srcfolder "${SECRET}" "${SECRET}"/secret.dmg
+  hdiutil create -encryption -srcfolder "${SECRET}" "${SECRET}"/secret.dmg
   rm "${PUB}"
   rm "${KEY}"
 fi
@@ -19,7 +19,7 @@ export PUB=${VOLUME}/pub.pem
 export KEY=${VOLUME}/key.pem
 
 if [[ ! -d ${VOLUME} ]]; then
-  hdiutil attach -stdinpass "${SECRET}"/secret.dmg
+  hdiutil attach "${SECRET}"/secret.dmg
 fi
 
 python3 "${FOLDER}"/manage.py

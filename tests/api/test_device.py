36da9669e00513ec9a10604f4288ba0f0bcc63f5
Attila Hornok <hornokattila@outlook.com>
2022-12-02 21:29:27 +0100

    Health check test 2

--- tests/api/test_device.py
+++ tests/api/test_device.py
@@ -1,12 +1,10 @@
-from gungnir.api.device import Device
+from gungnir.api.device import device
 
 
 class TestDevice:
-    device: Device = Device(lambda map: None)
-
     def test_health(self) -> None:
         result: bool = False
-        for detail in self.device.detail():
+        for detail in device.detail():
             if detail["rule"] == "/health":
                 result |= detail["view_func"]() == ""
         assert result

80abcff636717d40e8187dbab3811c8f0dbac786
Attila Hornok <hornokattila@outlook.com>
2022-12-16 20:39:06 +0100

    Fixed subset checking issue in settings.py

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ settings: typing.Dict[str, typing.Dict[str, typing.Union[int, str]]] = {
     "server": {
         "host": "0.0.0.0",
         "port": 443,
-        "ssl_context": (os.environ["PUB"], os.environ["KEY"]) if "PUB" and "KEY" in os.environ else "adhoc"
+        "ssl_context": (os.environ["PUB"], os.environ["KEY"]) if {"PUB", "KEY"}.issubset(os.environ) else "adhoc"
     },
     "system": {
         "groupId": "vaskaktusz",

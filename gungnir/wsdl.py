1a79ee45cf5670b9d7b5f892b3c3a8c4f1f949e6
Attila Hornok <hornokattila@outlook.com>
2023-01-20 01:15:55 +0100

    Renamed settings.

--- gungnir/wsdl.py
+++ gungnir/wsdl.py
@@ -6,14 +6,14 @@ from gungnir.utils.Blueprint import Blueprint
 
 
 class Flask(flask.Flask):
-    def run(self, **options: typing.Union[str, typing.Dict[str, typing.Union[int, str]], typing.List[Blueprint]]) -> None:
-        mirror: typing.List[Blueprint] = options["urls"]
+    def run(self, **settings: typing.Union[str, typing.Dict[str, typing.Dict[str, typing.Union[int, str, typing.Tuple[str, str]]]], typing.List[Blueprint]]) -> None:
+        mirror: typing.List[Blueprint] = settings["urls"]
         for rule in mirror:
-            rule.config = options["config"]
+            rule.config = settings["config"]
             rule.mirror = mirror
-            rule.system = options["system"]
+            rule.system = settings["system"]
             rule.launch()
             for detail in rule.detail():
                 rule.add_url_rule(**detail)
-            super().register_blueprint(rule, url_prefix=options["url_prefix"])
-        super().run(**options.pop("server"))
+            super().register_blueprint(rule, url_prefix=settings["url_prefix"])
+        super().run(**settings.pop("server"))

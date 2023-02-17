f9fe64bab0fce2cb2c15b7b9707965702b1780a2
Attila Hornok <hornokattila@outlook.com>
2023-02-17 10:44:45 +0100

    Added args to view_func call.

--- tests/TestUtil.py
+++ tests/TestUtil.py
@@ -3,7 +3,7 @@ from gungnir.utils.Blueprint import Blueprint
 
 class TestUtil:
     @staticmethod
-    def view_func(blueprint: Blueprint, rule: str) -> str:
+    def view_func(blueprint: Blueprint, rule: str, *args) -> str:
         for detail in blueprint.detail():
             if detail["rule"] == rule:
-                return detail["view_func"]()
+                return detail["view_func"](*args)

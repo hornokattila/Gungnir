6bbb47e0015a9e3e0c18a15336a1a88b77551157
Attila Hornok <hornokattila@outlook.com>
2023-02-17 10:49:54 +0100

    Parameterized args for TestUtil.py

--- tests/TestUtil.py
+++ tests/TestUtil.py
@@ -3,7 +3,7 @@ from gungnir.utils.Blueprint import Blueprint
 
 class TestUtil:
     @staticmethod
-    def view_func(blueprint: Blueprint, rule: str, *args) -> str:
+    def view_func(blueprint: Blueprint, rule: str, *args: ...) -> str:
         for detail in blueprint.detail():
             if detail["rule"] == rule:
                 return detail["view_func"](*args)

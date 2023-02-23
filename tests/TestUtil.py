4f95f6b8be060a392358236253228e3979aa0452
Attila Hornok <hornokattila@outlook.com>
2023-02-23 22:30:15 +0100

    Removed unused utility.

--- tests/TestUtil.py
+++ tests/TestUtil.py
@@ -7,8 +7,3 @@ class TestUtil:
         for detail in blueprint.detail():
             if rule == detail["rule"] and method in detail["methods"]:
                 return detail["view_func"](*args)
-
-    @staticmethod
-    def file():
-        with open(__file__, "r") as file:
-            return __file__, file.read()

3372f563c91f885033fb5b04ff68f77f7640e39e
Attila Hornok <hornokattila@outlook.com>
2023-03-07 11:17:11 +0100

    catch test util

--- tests/TestUtil.py
+++ tests/TestUtil.py
@@ -1,3 +1,5 @@
+import typing
+
 from gungnir.utils.Blueprint import Blueprint
 
 
@@ -7,3 +9,10 @@ class TestUtil:
         for detail in blueprint.detail():
             if rule == detail["rule"] and method in detail["methods"]:
                 return detail["view_func"](*args)
+
+    @staticmethod
+    def catch(method: typing.Callable[[], str]) -> typing.Union[str, typing.Type[Exception]]:
+        try:
+            return method()
+        except Exception as exception:
+            return exception.__class__

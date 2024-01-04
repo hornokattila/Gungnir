1d47d18146eb5120472720e52b6ac0a7469660ad
Attila Hornok <hornokattila@outlook.com>
2024-01-04 22:39:32 +0100

    3.12 upgrade

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setuptools.setup(
     install_requires=open("requirements.txt").readlines(),
     long_description=open("README.md").read(),
     packages=setuptools.find_packages(),
-    python_requires=">=3.11",
+    python_requires=">=3.12",
     **settings.settings["system"]
 )

9ee3ff96c48cb3ed259f51e86b40a737599a5565
Attila Hornok <hornokattila@outlook.com>
2024-01-06 01:57:03 +0100

    Revert "3.12 upgrade"
    This reverts commit 1d47d18146eb5120472720e52b6ac0a7469660ad.

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setuptools.setup(
     install_requires=open("requirements.txt").readlines(),
     long_description=open("README.md").read(),
     packages=setuptools.find_packages(),
-    python_requires=">=3.12",
+    python_requires=">=3.11",
     **settings.settings["system"]
 )

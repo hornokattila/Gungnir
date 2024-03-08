7953b110e5611fb9cab2850b253bd18156d85d08
Attila Hornok <hornokattila@outlook.com>
2024-03-08 10:26:46 +0100

    Refactor.

--- gungnir/api/bucket.py
+++ gungnir/api/bucket.py
@@ -12,8 +12,8 @@ class Bucket(Blueprint):
             {"rule": "/bucket/<folder>/<file>", "endpoint": "_delete_file", "view_func": _delete_file, "methods": ["DELETE"]},
             {"rule": "/bucket/<folder>", "endpoint": "_delete_files", "view_func": _delete_files, "methods": ["DELETE"]},
             {"rule": "/bucket/<folder>/<file>", "endpoint": "_file", "view_func": _file, "methods": ["GET"]},
-            {"rule": "/bucket/<folder>/<file>", "endpoint": "_put_file", "view_func": _put_file, "methods": ["PUT"]},
-            {"rule": "/bucket/<folder>", "endpoint": "_files", "view_func": _files, "methods": ["GET"]}
+            {"rule": "/bucket/<folder>", "endpoint": "_files", "view_func": _files, "methods": ["GET"]},
+            {"rule": "/bucket/<folder>/<file>", "endpoint": "_put_file", "view_func": _put_file, "methods": ["PUT"]}
         ]
 
 
@@ -34,15 +34,15 @@ def _file(folder: str, file: str) -> str:
     return bucket.flask.send_from_directory(os.path.join(bucket.config["bucket"], folder), file)
 
 
-def _put_file(folder: str, file: str) -> str:
-    path: str = os.path.join(bucket.config["bucket"], folder, bucket.werkzeug.utils.secure_filename(file))
-    with open(path, "wb") as file:
-        file.write(bucket.flask.request.data)
-    return ""
-
-
 def _files(folder: str) -> str:
     result: typing.Dict[str, typing.Dict[str, int]] = {}
     for file in os.scandir(os.path.join(bucket.config["bucket"], folder)):
         result[file.name] = dict(zip(("mode", "ino", "dev", "nlink", "uid", "gid", "size", "atime", "mtime", "ctime"), file.stat()))
     return bucket.flask.json.dumps(result)
+
+
+def _put_file(folder: str, file: str) -> str:
+    path: str = os.path.join(bucket.config["bucket"], folder, bucket.werkzeug.utils.secure_filename(file))
+    with open(path, "wb") as file:
+        file.write(bucket.flask.request.data)
+    return ""

b595066926c04f91e301f4ae27c1d531936e170a
Attila Hornok <hornokattila@outlook.com>
2024-04-13 02:51:44 +0200

    Externalized init method for deploy.py

--- gungnir/api/deploy.py
+++ gungnir/api/deploy.py
@@ -11,19 +11,20 @@ class Deploy(Blueprint):
     def detail(self: typing.Self) -> typing.List[typing.Dict[str, typing.Union[str, typing.Callable[..., str], typing.List[str]]]]:
         return [{"rule": "/deploy", "endpoint": "_put_script", "view_func": _put_script, "methods": ["PUT"]}]
 
+    @staticmethod
+    def initialize() -> str:
+        for folder in ["deploy", "logger", "upload"]:
+            os.makedirs(os.path.join(deploy.config["bucket"], folder), exist_ok=True)
+        return uuid.uuid4().hex
+
 
 deploy: Deploy = Deploy(LoginManager().shadow_loader)
 
 
 def _put_script() -> str:
-    for folder in ["deploy", "logger", "upload"]:
-        os.makedirs(os.path.join(deploy.config["bucket"], folder), exist_ok=True)
-    name: str = uuid.uuid4().hex
-
+    name: str = deploy.initialize()
     def _path_finder(folder: str) -> str: return os.path.join(deploy.config["bucket"], folder)
-
     def _file_finder(folder: str) -> str: return os.path.join(_path_finder(folder), name)
-
     file: str = _file_finder("deploy")
     with open(file, "wb") as script:
         script.write(deploy.flask.request.data)

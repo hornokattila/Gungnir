8744cbdacd44871039817636645a130211779c8a
Attila Hornok <hornokattila@outlook.com>
2024-06-04 13:24:40 +0200

    Renamed log handler.

--- gungnir/utils/LogHandler.py
+++ gungnir/utils/LogHandler.py
@@ -1,21 +1,21 @@
 import logging
 import os
 import typing
-from logging import handlers
+from logging.handlers import TimedRotatingFileHandler
 
 
-class Logger:
+class LogHandler:
     config: typing.Dict[str, typing.Union[int, str, typing.Dict[str, typing.Union[int, str]]]] = {}
 
     def __init__(self: typing.Self, settings: typing.Dict[str, typing.Union[int, str]]) -> None:
-        Logger.config.update(settings)
-        os.makedirs(os.path.dirname(Logger.config["handler"]["filename"]), exist_ok=True)
-        self.handler: logging.FileHandler = handlers.TimedRotatingFileHandler(**Logger.config["handler"])
-        self.handler.setFormatter(logging.Formatter(Logger.config["format"]))
+        LogHandler.config.update(settings)
+        os.makedirs(os.path.dirname(LogHandler.config["handler"]["filename"]), exist_ok=True)
+        self.handler: logging.FileHandler = TimedRotatingFileHandler(**LogHandler.config["handler"])
+        self.handler.setFormatter(logging.Formatter(LogHandler.config["format"]))
 
     def basic_config(self: typing.Self, logger: logging.Logger) -> None:
         logger.addHandler(self.handler)
-        logger.setLevel(Logger.config["level"])
+        logger.setLevel(LogHandler.config["level"])
 
     def get_logger(self: typing.Self, name: str) -> logging.Logger:
         logger: logging.Logger = logging.getLogger(name)
--- manage.py
+++ manage.py
@@ -2,9 +2,9 @@ import logging
 
 import settings
 from gungnir import urls
-from gungnir import wsdl
-from gungnir.utils.LogHandler import Logger
+from gungnir.utils.LogHandler import LogHandler
+from gungnir.wsdl import Flask
 
 if __name__ == "__main__":
-    Logger(settings.settings.pop("logger")).basic_config(logging.getLogger())
-    wsdl.Flask(__name__).run(**settings.settings, errors=urls.errors, urls=urls.urls, url_prefix=urls.url_prefix)
+    LogHandler(settings.settings.pop("logger")).basic_config(logging.getLogger())
+    Flask(__name__).run(**settings.settings, errors=urls.errors, urls=urls.urls, url_prefix=urls.url_prefix)

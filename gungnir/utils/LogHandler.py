36466a0c5ae8e1b7aa27f8457d59e582617abcde
Attila Hornok <hornokattila@outlook.com>
2024-08-13 13:53:10 +0200

    LogHandler back to default. Still no final decision on how to use.

--- gungnir/utils/LogHandler.py
+++ gungnir/utils/LogHandler.py
@@ -5,19 +5,15 @@ from logging.handlers import TimedRotatingFileHandler
 
 
 class LogHandler:
-    config: typing.Dict[str, typing.Union[int, str, typing.Dict[str, typing.Union[int, str]]]] = {}
-
-    def __init__(self: typing.Self, settings: typing.Dict[str, typing.Union[int, str]]) -> None:
-        LogHandler.config.update(settings)
-        os.makedirs(os.path.dirname(LogHandler.config["handler"]["filename"]), exist_ok=True)
-        self.handler: logging.FileHandler = TimedRotatingFileHandler(**LogHandler.config["handler"])
-        self.handler.setFormatter(logging.Formatter(LogHandler.config["format"]))
-
-    def basic_config(self: typing.Self, logger: logging.Logger) -> None:
-        logger.addHandler(self.handler)
-        logger.setLevel(LogHandler.config["level"])
+    # TODO: FIXME!
+    def __init__(self: typing.Self, settings: typing.Dict[str, typing.Union[int, str, typing.Dict[str, typing.Union[int, str]]]]) -> None:
+        os.makedirs(os.path.dirname(settings["handler"]["filename"]), exist_ok=True)
+        self.handler = TimedRotatingFileHandler(**settings["handler"])
+        self.handler.setFormatter(logging.Formatter(settings["format"]))
+        self.handler.setLevel(settings["level"])
 
     def get_logger(self: typing.Self, name: str) -> logging.Logger:
         logger: logging.Logger = logging.getLogger(name)
-        self.basic_config(logger)
+        logger.addHandler(self.handler)
+        logger.setLevel(self.handler.level)
         return logger
--- manage.py
+++ manage.py
@@ -1,10 +1,6 @@
-import logging
-
 import settings
 from gungnir import urls
-from gungnir.utils.LogHandler import LogHandler
 from gungnir.wsdl import Flask
 
 if __name__ == "__main__":
-    LogHandler(settings.settings.pop("logger")).basic_config(logging.getLogger())
     Flask(__name__).run(**settings.settings, errors=urls.errors, urls=urls.urls, url_prefix=urls.url_prefix)
